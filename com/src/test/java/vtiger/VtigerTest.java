883f89b871b4830bf5199502c7484017eedb0420
admin <admin@DESKTOP-VC14B0M>
2022-07-19 11:45:39 +0530

    moving to local repository

---
+++ com/src/test/java/vtiger/VtigerTest.java
@@ -0,0 +1,23 @@
+package vtiger;
+
+import org.testng.annotations.Test;
+
+public class VtigerTest {
+	
+	@Test
+	public void CreateOrganisationTest()
+	{
+		System.out.println("create organisation is done");
+	}
+	@Test
+	public void CreateContactsTest()
+	{
+		System.out.println("create contacts is done");
+	}
+	@Test
+	public void CreateProductsTest()
+	{
+		System.out.println("create products is done");
+	}
+
+}
