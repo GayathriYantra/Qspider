20368c7551b6b1592fd3c0ca45ff1b96a139e1f8
admin <admin@DESKTOP-VC14B0M>
2022-07-20 16:56:24 +0530

    7th commit

--- com/src/test/java/vtiger/VtigerTest.java
+++ com/src/test/java/vtiger/VtigerTest.java
@@ -18,7 +18,7 @@ public class VtigerTest {
 	public void CreateProductsTest()
 	{
 		//modification is done
-		//parameters
+		//parameters passing
 		System.out.println("create products is done");
 		System.out.println("product success");
 		System.out.println("product");
